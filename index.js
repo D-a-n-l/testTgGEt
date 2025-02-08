1146b5c8b16a7500e5433be115528c9cb29e86a0
Daniil Oleynikov <danilachka225@gmail.com>
2025-02-08 14:42:22 +0300

    jfgh

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Открыть игру:", {
         reply_markup: {
             inline_keyboard: [
-                [{ text: "Играть!", web_app: { url: "https://d-a-n-l.github.io/testTgGEt/" } }]
+                [{ text: msg.from_user.id, web_app: { url: "https://d-a-n-l.github.io/testTgGEt/" } }]
             ]
         }
     });
