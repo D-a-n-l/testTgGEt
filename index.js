c5505f86f585f06a21744390f94431cf1c55a55e
Daniil Oleynikov <danilachka225@gmail.com>
2025-02-08 18:05:43 +0300

    Test url username

--- index.js
+++ index.js
@@ -4,25 +4,21 @@ const TelegramBot = require("node-telegram-bot-api");
 const app = express();
 app.use(express.json());
 
-const TOKEN = "AAF9bPKkb3Uqkd_X6ZoxmDMtqCWAvBs4U_E";
+const TOKEN = "";
 const bot = new TelegramBot(TOKEN);
 
-let users = {}; // Словарь для хранения { username: user_id }
+app.post("/", (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
 
-// Обрабатываем /start и сохраняем user_id
 bot.onText(/\/start/, (msg) => {
-    const user_id = msg.from.id;
-    const username = msg.from.username;
+    const username = msg.from.username || "";
 
-    if (username) {
-        users[username.toLowerCase()] = user_id;
-        console.log(`Сохранен: @${username} -> ${user_id}`);
-    }
-
-    bot.sendMessage(user_id, "Открыть игру:", {
+    bot.sendMessage(msg.chat.id, "Открыть игру:", {
         reply_markup: {
             inline_keyboard: [
-                [{ text: username, web_app: { url: "https://d-a-n-l.github.io/testTgGEt/" } }]
+                [{ text: "Играть", web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } }]
             ]
         }
     });
