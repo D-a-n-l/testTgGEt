26bd905ee8ddfb6ccb2438a3a6a2e9436141cc19
Daniil Oleynikov <danilachka225@gmail.com>
2025-02-15 22:44:00 +0300

    h;gfd h'g;flhg

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const TOKEN = "6436341565:AAF9bPKkb3Uqkd_X6ZoxmDMtqCWAvBs4U_E";
 
 const bot = new TelegramBot(TOKEN, { webHook: true });
 
-const URL = 'https://test-tg-g-et.vercel.app/';
-bot.setWebHook(`${URL}/api/bot${TOKEN}`);
 
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
@@ -22,15 +20,58 @@ module.exports = async (req, res) => {
 
 
 bot.onText(/\/start/, (msg) => {
-    const username = msg.from.username || "";
-    
+    const chatId = msg.chat.id;
+
     const keyboard = {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Play', web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } }]
+                [{ text: 'Нажми меня', callback_data: 'change_buttons' }]
             ]
         }
     };
 
-    bot.sendMessage(msg.chat.id, 'Выберите кнопку:', keyboard);
+    bot.sendMessage(chatId, 'Нажмите кнопку ниже:', keyboard);
+});
+
+// Обработка нажатий на inline-кнопки
+bot.on('callback_query', async (query) => {
+    const chatId = query.message.chat.id;
+
+    if (query.data === 'change_buttons') {
+        const newKeyboard = {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Кнопка 1', callback_data: 'btn1' },
+                        { text: 'Кнопка 2', callback_data: 'btn2' }
+                    ],
+                    [{ text: 'Назад', callback_data: 'back' }]
+                ]
+            }
+        };
+
+        await bot.editMessageText('Выберите новую кнопку:', {
+            chat_id: chatId,
+            message_id: query.message.message_id,
+            reply_markup: newKeyboard.reply_markup
+        });
+    }
+
+    if (query.data === 'back') {
+        const originalKeyboard = {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Нажми меня', callback_data: 'change_buttons' }]
+                ]
+            }
+        };
+
+        await bot.editMessageText('Нажмите кнопку ниже:', {
+            chat_id: chatId,
+            message_id: query.message.message_id,
+            reply_markup: originalKeyboard.reply_markup
+        });
+    }
+
+    await bot.answerCallbackQuery(query.id); // Убираем значок загрузки на кнопке
 });
