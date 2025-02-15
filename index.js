54d1e71613abacb88d8e2eb8364c832e984395a4
Daniil Oleynikov <danilachka225@gmail.com>
2025-02-15 17:05:37 +0300

    test architecture butons

--- index.js
+++ index.js
@@ -13,26 +13,94 @@ app.post("/", (req, res) => {
 });
 
 bot.onText(/\/start/, (msg) => {
-    const username = msg.from.username || "";
-    
     const keyboard = {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'DeepLift', callback_data: 'btn1'},
-                    { text: 'Description', callback_data: 'btn1' },
-                    { text: 'Play', web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } }
+                    { text: 'DeepLift', callback_data: 'DeepLift'}
                 ],
                 [
-                    { text: 'Кнопка 1', callback_data: 'btn1' },
-                    { text: 'Кнопка 2', callback_data: 'btn2' }
-                ]
+                    { text: 'StreetPigeon', callback_data: 'StreetPigeon'}
+                ],
             ]
         }
     };
 
-    bot.sendMessage(msg.chat.id, 'Выберите кнопку:', keyboard);
+    bot.sendMessage(msg.chat.id, 'Выберите игру:', keyboard);
 });
 
+// Обработка нажатий на inline-кнопки
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+
+    if (query.data === 'DeepLift') {
+        const username = msg.from.username || "";
+
+        const newKeyboard = {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Description', callback_data: 'btn1' },
+                        { text: 'Play', web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } }
+                    ],
+                    [
+                        { text: 'Back', callback_data: 'Back' },
+                    ]
+                ]
+            }
+        };
+
+        bot.editMessageText('Выберите действие:', {
+            chat_id: chatId,
+            message_id: query.message.message_id,
+            reply_markup: newKeyboard.reply_markup
+        });
+    }
+
+    if (query.data === 'StreetPigeon') {
+        const newKeyboard = {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Description', callback_data: 'btn1' },
+                        { text: 'Play', callback_data: 'btn2' }
+                    ],
+                    [
+                        { text: 'Back', callback_data: 'Back' },
+                    ]
+                ]
+            }
+        };
+
+        bot.editMessageText('Выберите действие:', {
+            chat_id: chatId,
+            message_id: query.message.message_id,
+            reply_markup: newKeyboard.reply_markup
+        });
+    }
+
+    if (query.data === 'Back') {
+        const originalKeyboard = {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'DeepLift', callback_data: 'DeepLift'}
+                    ],
+                    [
+                        { text: 'StreetPigeon', callback_data: 'StreetPigeon'}
+                    ],
+                ]
+            }
+        };
+
+        bot.editMessageText('Нажмите кнопку ниже:', {
+            chat_id: chatId,
+            message_id: query.message.message_id,
+            reply_markup: originalKeyboard.reply_markup
+        });
+    }
+
+    bot.answerCallbackQuery(query.id);
+});
 
 module.exports = app;
