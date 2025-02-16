65ec498924d84084fb163b6e98898468409d3aa1
Daniil Oleynikov <danilachka225@gmail.com>
2025-02-16 12:33:39 +0300

    Back to past

--- index.js
+++ index.js
@@ -1,105 +1,30 @@
+const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
 
-const TOKEN = "6436341565:AAF9bPKkb3Uqkd_X6ZoxmDMtqCWAvBs4U_E";
-const bot = new TelegramBot(TOKEN, { webHook: true });
+const app = express();
+app.use(express.json());
 
-module.exports = async (req, res) => {
-    if (req.method === 'POST') {
-        res.status(200).end(); // Отвечаем сразу, чтобы Telegram не повторял запрос
-        process.nextTick(() => bot.processUpdate(req.body));
-    } else {
-        res.status(200).send('Hello from Telegram Bot');
-    }
-};
+const TOKEN = "6436341565:AAF9bPKkb3Uqkd_X6ZoxmDMtqCWAvBs4U_E";
+const bot = new TelegramBot(TOKEN);
+
+app.post("/", (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
 
 bot.onText(/\/start/, (msg) => {
     const username = msg.from.username || "";
-
+    
     const keyboard = {
         reply_markup: {
             inline_keyboard: [
-                [
-                    { text: 'Play', web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } },
-                    { text: 'DeepLift', callback_data: 'DeepLift'}
-                ],
-                [
-                    { text: 'StreetPigeon', callback_data: 'StreetPigeon'}
-                ],
+                [{ text: 'Play', web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } }]
             ]
         }
     };
 
-    bot.sendMessage(msg.chat.id, 'Выберите игру:', keyboard);
+    bot.sendMessage(msg.chat.id, 'DeepLift', keyboard);
 });
 
-bot.on('callback_query', (query) => {
-    const chatId = query.message.chat.id;
-    const username = query.from.username || "";
 
-    if (query.data === 'DeepLift') {
-        const newKeyboard = {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        { text: 'Description', callback_data: 'btn1' },
-                        { text: 'Play', web_app: { url: `https://d-a-n-l.github.io/testTgGEt/?username=${username}` } }
-                    ],
-                    [
-                        { text: 'Back', callback_data: 'Back' },
-                    ]
-                ]
-            }
-        };
-
-        bot.editMessageText('Выберите действие:', {
-            chat_id: chatId,
-            message_id: query.message.message_id,
-            reply_markup: newKeyboard.reply_markup
-        });
-    }
-
-    if (query.data === 'StreetPigeon') {
-        const newKeyboard = {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        { text: 'Description', callback_data: 'btn1' },
-                        { text: 'Play', callback_data: 'btn2' }
-                    ],
-                    [
-                        { text: 'Back', callback_data: 'Back' },
-                    ]
-                ]
-            }
-        };
-
-        bot.editMessageText('Выберите действие:', {
-            chat_id: chatId,
-            message_id: query.message.message_id,
-            reply_markup: newKeyboard.reply_markup
-        });
-    }
-
-    if (query.data === 'Back') {
-        const originalKeyboard = {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        { text: 'DeepLift', callback_data: 'DeepLift'}
-                    ],
-                    [
-                        { text: 'StreetPigeon', callback_data: 'StreetPigeon'}
-                    ],
-                ]
-            }
-        };
-
-        bot.editMessageText('Нажмите кнопку ниже:', {
-            chat_id: chatId,
-            message_id: query.message.message_id,
-            reply_markup: originalKeyboard.reply_markup
-        });
-    }
-
-    bot.answerCallbackQuery(query.id);
-});
+module.exports = app;
